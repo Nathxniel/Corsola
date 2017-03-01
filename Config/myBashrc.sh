7fda7e87448b232098a3d75dee9f9c89c6e5677d
Oluwaseun Oshunniyi <ono16@ray29.doc.ic.ac.uk>
2017-03-01 12:23:12 +0000

    added aliases

--- Config/myBashrc.sh
+++ Config/myBashrc.sh
@@ -265,6 +265,11 @@ fi
 # static aliases
 alias c='clear'
 alias battery='upower -i /org/freedesktop/UPower/devices/battery_BAT1'
+alias corsola='cd ~/Documents/Corsola/'
+alias settings='corsola && cd Config'
+alias editbash='settings && vim myBashrc.sh'
+alias editi3='settings && vim myi3Config/config'
+alias editi3bar='settings && vim myi3Config/i3status.conf'
 
 # variant aliases (TODO)
 alias now='cd $(ls -td -- ~/Documents/*/ | head -n 1)'
