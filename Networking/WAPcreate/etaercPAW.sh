3d901d85478989b46d4f068203d7dc5ecf544447
nathaniel <oluwaseun.oshunniyi16@imperial.ac.uk>
2017-11-10 17:31:26 +0000

    ssid change and stop script update for dns 1

--- Networking/WAPcreate/etaercPAW.sh
+++ Networking/WAPcreate/etaercPAW.sh
@@ -2,14 +2,24 @@
 # TODO: generalise
 
 # delete all wireless interfaces
-iw dev wlp3s0_sta del
-iw dev wlp3s0_ap del
+#   this is rude and unnecessary
+if ! [ -z $1 ]; then
+  iw dev wlp3s0_sta del
+  iw dev wlp3s0_ap del;
+else
+  ip link set wlp3s0_sta down
+  ip link set wlp3s0_ap down;
+fi
+
 
 # up standard wireless interface
 ip link set wlp3s0 up
 
 # revert network interfaces file
 cat resources/defaultInterfaces > /etc/network/interfaces
+
+# stop bind service
+service bind9 stop
 
 # start network manager
 service network-manager start
